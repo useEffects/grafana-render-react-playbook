e53b43342d6f292f0833c789af3545890f02a309
useEffects <joel.sr1024@gmail.com>
2023-10-26 01:36:20 +0530

    init

---
+++ src/index.ts
@@ -0,0 +1,68 @@
+import { readFileSync, watch } from "fs";
+import { exit } from "process";
+import debounce from "debounce";
+import "dotenv/config"
+
+const { API_KEY, ORG_ID, DASHBOARD_UID, PANEL_ID, HOSTNAME, FOLDER_UID } = process.env
+
+const filePath = __dirname + "/source.tsx";
+
+function getFileContents() {
+    const fileContent = readFileSync(filePath, "utf8");
+    const startComment = "// <- Start Here ->";
+    const startIndex = fileContent.indexOf(startComment);
+    if (startIndex === -1) {
+        console.log("Could not find the start comment");
+        exit();
+    }
+    const extractedContent = fileContent.substring(startIndex + startComment.length).trim();
+    return extractedContent
+}
+
+async function postChanges() {
+    const sourceCode = getFileContents()
+    const response = await fetch(`http://${HOSTNAME}/api/dashboards/uid/${DASHBOARD_UID}`, {
+        method: "GET",
+        headers: {
+            "Authorization": `Bearer ${API_KEY}`
+        }
+    })
+    const result = await response.json()
+    const { dashboard } = result
+    dashboard.panels = result.dashboard.panels.map((panel: any) => {
+        if (panel.id === parseInt(PANEL_ID!)) {
+            return {
+                ...panel,
+                options: {
+                    code: sourceCode
+                }
+            }
+        }
+        else return panel
+    })
+    const payload = {
+        dashboard,
+        message: `developer changes`,
+        overwrite: true,
+        folderUid: FOLDER_UID
+    }
+    const postResponse = await fetch(`http://${HOSTNAME}/api/dashboards/db`, {
+        method: "POST",
+        headers: {
+            "Authorization": `Bearer ${API_KEY}`,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(payload)
+    })
+    if (postResponse.status !== 200) {
+        console.log(`status ${postResponse.status} ${postResponse.statusText}`)
+    }
+}
+
+const debouncedPostChanges = debounce(postChanges, 500);
+
+watch(filePath, (eventName) => {
+    if (eventName === "change") {
+        debouncedPostChanges();
+    }
+});
---
+++ src/source.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const render = (fc: React.JSX.Element) => { }
+
+// <- Start Here ->
+
+function App() {
+    return <div>
+        <p> Hello World </p>
+    </div>
+}
+render(<App />)
